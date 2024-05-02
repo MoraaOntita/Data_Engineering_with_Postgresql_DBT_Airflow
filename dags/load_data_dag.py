b9ebff72aa56eb5645d658871111179da1fe05ee
MoraaOntita <moraaontita1844@gmail.com>
2024-05-02 16:14:56 +0300

    Updated the project structure and modified the dags and logs

--- dags/load_data_dag.py
+++ dags/load_data_dag.py
@@ -1,5 +1,11 @@
-from airflow.decorators import dag, task
 from datetime import datetime
+from airflow import DAG
+from airflow.decorators import dag, task
+from airflow.operators.python import PythonOperator
+import sys
+
+sys.path.append('/media/moraa/New Volume/Ontita/10Academy/Cohort B/Projects/Week2/Data_Engineering_with_Postgresql_DBT_Airflow')
+
 from scripts.config import (
     csv_file_path,
     prod_postgres_conn,
@@ -8,28 +14,34 @@ from scripts.config import (
 )
 from utils import load_data_to_postgres
 
+
+default_args = {
+    'owner': 'airflow',
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'start_date': datetime(2024, 4, 30),
+    'catchup': False
+}
+
 @dag(
-    'data_load_dag',
-    schedule_interval='*/10 * * * *',  # Run every 10 minutes
-    start_date=datetime(2024, 4, 30),  # A day before the desired start date
-    catchup=False,
-    default_args={
-        'owner': 'airflow',
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-    }
+    dag_id='data_load_dag',
+    schedule_interval='*/10 * * * *',  
+    default_args=default_args,
+    description='Data loading DAG with PythonOperator'
 )
-def data_load_dag():
-    @task()
+
+
+def data_load_dag_with_python_operator():
+    @task(task_id='load_data_to_prod', dag=dag)
     def load_data_to_prod():
         load_data_to_postgres(csv_file_path, prod_postgres_conn, 'prod_track_table', 'prod_trajectory_table')
 
-    @task()
+    @task(task_id='load_data_to_dev', dag=dag)
     def load_data_to_dev():
         load_data_to_postgres(csv_file_path, dev_postgres_conn, 'dev_track_table', 'dev_trajectory_table')
 
-    @task()
+    @task(task_id='load_data_to_staging', dag=dag)
     def load_data_to_staging():
         load_data_to_postgres(csv_file_path, staging_postgres_conn, 'staging_track_table', 'staging_trajectory_table')
 
@@ -39,4 +51,6 @@ def data_load_dag():
 
     load_data_to_prod_task >> [load_data_to_dev_task, load_data_to_staging_task]
 
-dag = data_load_dag()
+dag = data_load_dag_with_python_operator()
+
+
---
+++ tests/run_test.py
@@ -0,0 +1,41 @@
+import unittest
+from datetime import datetime
+from airflow.models import DagBag
+
+class TestDataLoadDag(unittest.TestCase):
+    def setUp(self):
+        self.dagbag = DagBag(include_examples=False)  # Load the DAGs without example DAGs
+
+    def test_dag_loaded(self):
+        """Check if the DAG is loaded successfully"""
+        dag_id = 'data_load_dag'
+        dag = self.dagbag.get_dag(dag_id)
+        self.assertIsNotNone(dag)
+
+    def test_task_count(self):
+        """Check the number of tasks in the DAG"""
+        dag_id = 'data_load_dag'
+        dag = self.dagbag.get_dag(dag_id)
+        self.assertIsNotNone(dag)
+        self.assertEqual(len(dag.tasks), 3)  # Assuming you have three tasks
+
+    def test_task_dependencies(self):
+        """Check the task dependencies in the DAG"""
+        dag_id = 'data_load_dag'
+        dag = self.dagbag.get_dag(dag_id)
+        self.assertIsNotNone(dag)
+
+        # Check task dependencies
+        task_dependencies = dag.get_task_instances(datetime.now())
+        expected_dependencies = {
+            'load_data_to_prod': ['load_data_to_dev', 'load_data_to_staging']
+        }
+        for task_id, upstream_task_ids in expected_dependencies.items():
+            task = dag.get_task(task_id)
+            upstream_tasks = task.get_direct_relatives(upstream=True)
+            upstream_task_ids_actual = [t.task_id for t in upstream_tasks]
+            self.assertCountEqual(upstream_task_ids_actual, upstream_task_ids)
+
+if __name__ == '__main__':
+    unittest.main()
+
