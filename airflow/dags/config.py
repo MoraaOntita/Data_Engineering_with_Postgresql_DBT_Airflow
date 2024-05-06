a145940123281de582c93d35fc2db879c160785b
MoraaOntita <moraaontita1844@gmail.com>
2024-05-06 12:09:57 +0300

    Created new files in airflow and dbt project

---
+++ airflow/dags/config.py
@@ -0,0 +1,5 @@
+# config.py
+
+# Define configuration settings
+csv_file_path = '/media/moraa/New Volume/Ontita/10Academy/Cohort B/Projects/Week2/Data_Engineering_with_Postgresql_DBT_Airflow/Data/20181024_d1_0830_0900.csv'
+postgres_conn = 'wk2_postgres'
