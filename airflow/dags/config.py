e8c6900ac81bc6b79643eed96d31670e79a3eb9e
MoraaOntita <moraaontita1844@gmail.com>
2024-05-06 06:46:34 +0300

    modified the airflow and created a new file in the scripts folder

--- airflow/dags/config.py
+++ airflow/dags/config.py
@@ -2,4 +2,4 @@
 
 # Define configuration settings
 csv_file_path = '/media/moraa/New Volume/Ontita/10Academy/Cohort B/Projects/Week2/Data_Engineering_with_Postgresql_DBT_Airflow/Data/20181024_d1_0830_0900.csv'
-postgres_conn = 'your_postgres_connection_string'
+postgres_conn = 'wk2_postgres'
